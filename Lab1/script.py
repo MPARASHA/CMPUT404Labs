cf21ba85314033c873be439523da8cc917d5dcb0
Neil <mparasha@ualberta.ca>
2021-09-04 09:33:46 -0600

    Lab 1 Complete

--- Lab1/script.py
+++ Lab1/script.py
@@ -1,8 +1,11 @@
 import requests 
 
-print("Version of Library: "+ requests.__version__)
-url = "https://google.com"
-res = requests.get(url)
+print("\nVersion of Requests Library: "+ requests.__version__)
+urlGoogle = "https://google.com"
+resGoogle = requests.get(urlGoogle)
 
-print("\nHTML Status Code: "+ str(res.status_code))
-print("\nBody of Request: \n\n" + res.text)
+urlScript = "https://raw.githubusercontent.com/MPARASHA/CMPUT404Labs/master/Lab1/script.py"
+resScript = requests.get(urlScript)
+
+print("\nHTML Status Code: "+ str(resScript.status_code))
+print("\nBody of Request: \n\n" + resScript.text)
