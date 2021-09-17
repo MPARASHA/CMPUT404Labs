63b070b6d9b2ea891efab60cc0e2d28f20273416
Neil <mparasha@ualberta.ca>
2021-09-17 10:52:58 -0600

    implemented multi proxy server

--- Lab2/multi_proxy_client.py
+++ Lab2/multi_proxy_client.py
@@ -32,7 +32,7 @@ def main():
     address = [(HOST, PORT)]
     #establish 3 different connections
     with Pool() as p:
-        p.map(connect, address * 10)
+        p.map(connect, address * 3)
 
 if __name__ == "__main__":
     main()
---
+++ Lab2/multi_proxy_server.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import socket
+import time
+from multiprocessing import Process
+
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+#get ip/host information
+def get_remote_ip(host):
+	print(f'Getting IP for {host}')
+	try:
+		remote_ip = socket.gethostbyname(host)
+	except socket.gaierror:
+		print('Hostname could not be resolved. Exiting')
+		sys.exit()
+	print(f'Ip address of {host} is {remote_ip}')
+	return remote_ip
+
+def main():
+    #create socket, bind, and set to listening mode
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as proxy_start:
+        print("Start multi proxy server")
+        #allow reused addresses
+        proxy_start.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+        proxy_start.bind((HOST,PORT))
+        proxy_start.listen(2)
+
+        while True:
+            #accept connections and start a Process daemon for handling multiple connections
+            conn,addr = proxy_start.accept()
+            p = Process(target=handle_request, args=(addr,conn))
+            p.daemon=True
+            p.start()
+            print("Started process ", p)
+
+#echo connections back to client
+def handle_request(addr, conn):
+    print("connected by", addr)
+    extern_host = 'www.google.com'
+    extern_port = 80
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end:
+        print("Connecting to Google")
+        remote_ip = get_remote_ip(extern_host)
+
+        #connect proxy_end
+        proxy_end.connect((remote_ip, extern_port))
+
+        #send data'
+        send_full_data = conn.recv(BUFFER_SIZE)
+        print(f"Sending received data {send_full_data} to google")
+        proxy_end.sendall(send_full_data)
+        
+        #remember to shut down!!
+        proxy_end.shutdown(socket.SHUT_WR)
+        
+        data = proxy_end.recv(BUFFER_SIZE)
+        print(f"Sending received data {data} to client")
+        #send data back
+        conn.send(data)
+    
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close()
+
+
+if __name__ =="__main__":
+    main()
